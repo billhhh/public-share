5461a87b51173ed292ea62d3bfe7cead770177c0
Bill Wang <billhhh@users.noreply.github.com>
2022-03-23 13:30:06 +1030

    Update rm_punct.py

--- rm_punct.py
+++ rm_punct.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import string
 
-bash_path = '/Path_To_Files/'
-input_file = bash_path + 'pg100.txt'
-output_file = bash_path + 'pg100_nopunct.txt'
+base_path = '/Path_To_Files/'
+input_file = base_path + 'pg100.txt'
+output_file = base_path + 'pg100_nopunct.txt'
 
 filepath = Path(input_file)
 text = filepath.read_text()
